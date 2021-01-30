8a217e05fbd1bf9b2b0576dab73cdd7278abae3d
AngeloYuka <rosilenecabralsouza@gmail.com>
2021-01-30 02:06:11 -0300

    First commit

---
+++ Dark-main/src/darkmenu.js
@@ -0,0 +1,40 @@
+const angmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do AnG:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *BOT*
+  DUVIDAS? 👇
+  WA.me/+559193312057
+╚════════════════════`
+}
+
+exports.angmenu = angmenu
+
+
+
+
+
+
+
+
